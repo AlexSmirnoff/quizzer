390c4a2937e7a576a81c4fa7e6b5180899a613ee
Alex <alexsmirwork@outlook.com>
2024-03-04 22:51:26 +0000

    add chain rounds

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizRoundComponent } from './general/quiz-round/quiz-round.component';
+import { QuizRoundChainComponent } from './general/quiz-round-chain/quiz-round-chain.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'default', component: QuizRoundComponent},
+  {path: 'chain', component: QuizRoundChainComponent},
+  {path: '', redirectTo: 'default', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/general/answer-field/answer-field.component.ts
+++ src/app/general/answer-field/answer-field.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import {AnswerState} from "src/app/classes/answer-state"
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-answer-field',
@@ -8,9 +7,12 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./answer-field.component.css']
 })
 export class AnswerFieldComponent {
-  state = AnswerState.CORRECT;
+  private state = AnswerState.CORRECT;
 
-  private static stateMap= new Map([
+  @Output() changed = new EventEmitter<AnswerState>();
+
+  @Input()
+  stateMap = new Map([
     [AnswerState.CORRECT, AnswerState.WRONG],
     [AnswerState.WRONG, AnswerState.EMPTY],
     [AnswerState.EMPTY, AnswerState.CORRECT]
@@ -19,7 +21,7 @@ export class AnswerFieldComponent {
   @Input() multiplier = 1;
 
   public onClick() {
-    this.state = AnswerFieldComponent.stateMap.get(this.state) ?? AnswerState.CORRECT;
+    this.setState(this.stateMap.get(this.state) ?? AnswerState.CORRECT);
   }
 
   public getCurrentStyle(): string {
@@ -36,5 +38,6 @@ export class AnswerFieldComponent {
 
   public setState(newState: AnswerState): void {
     this.state = newState;
+    this.changed.emit(newState);
   }
 }
